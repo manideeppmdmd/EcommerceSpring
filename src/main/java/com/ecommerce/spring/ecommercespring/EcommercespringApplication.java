cea636d8afd7e0b95bfa4dc14648005f3fdbde4d
Manideep Pilli <manideep.pmd@gmail.com>
2025-10-28 15:37:59 -0600

    ADDED: .env file and configured it

--- src/main/java/com/ecommerce/spring/ecommercespring/EcommercespringApplication.java
+++ src/main/java/com/ecommerce/spring/ecommercespring/EcommercespringApplication.java
@@ -1,13 +1,22 @@
 package com.ecommerce.spring.ecommercespring;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EcommercespringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommercespringApplication.class, args);
-	}
+  public static void main(String[] args) {
+    Dotenv dotenv = Dotenv.configure().load();
 
+    dotenv
+      .entries()
+      .forEach((DotenvEntry entry) ->
+        System.setProperty(entry.getKey(), entry.getValue())
+      );
+
+    SpringApplication.run(EcommercespringApplication.class, args);
+  }
 }
