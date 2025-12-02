f45d758c47af1e7893fff9b1ec332b395ee85ef2
Manideep Pilli <manideep.pmd@gmail.com>
2025-12-01 21:15:49 -0700

    ADDED: Eureka service discovery

--- src/main/java/com/ecommerce/spring/ecommercespring/EcommercespringApplication.java
+++ src/main/java/com/ecommerce/spring/ecommercespring/EcommercespringApplication.java
@@ -1,13 +1,16 @@
 package com.ecommerce.spring.ecommercespring;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.github.cdimascio.dotenv.DotenvEntry;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class EcommercespringApplication {
 
   public static void main(String[] args) {
